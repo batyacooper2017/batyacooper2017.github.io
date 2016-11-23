501692cd9123919539437e835af6c61cfc2bac0a
batyacooper2017 <batyacooper@gmail.com>
2016-11-23 02:08:59 +0200

    new work

---
+++ assignments/class3/assets/jquery.js
@@ -0,0 +1,48 @@
+   // $("footer").hover(
+     //  function(){
+      //      $(this).fadeIn(100);
+      //  },
+     //   function(){
+     //      $(this).fadeOut(100);
+     //   }
+
+  //  );
+
+
+   // $("footer").css({'opacity':0}).hover(
+     //   function(){
+      //      $(this).animate({'opacity':1},100);
+      //  },
+     //   function(){
+      //      $(this).animate({'opacity':0},100);
+    //    }
+  //  );
+    //$("button").click(function(){
+       // $("footer").hide();
+  //  });
+
+    //var captionHidden = true;
+
+    //$("footer").on("click", function(){
+
+        //if (captionHidden){
+          //   $(this).next().show()
+       //  captionHidden = false;
+      //  }else{
+          //   $(this).next().hide()
+          //  captionHidden = true;
+     //   }
+
+  //  });
+
+   //var myFooter = $("footer");
+    //var myCaption = myImg.next();
+
+   // myImg.on("mouseenter", function(){
+       // myCaption.slideDown();
+   // }).on("mouseleave", function(){
+       // myCaption.slideUp();
+   // });
+
+//});
+
